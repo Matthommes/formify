c766fcdf519fe0d859b2c922075f783e64262e6d
Matthew Akahomen <alexislordqtest@gmail.com>
2024-09-12 12:16:36 +0100

    Fix deployment error

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,25 @@
+// app/api/auth/register/route.ts
+
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (user) {
+      return NextResponse.json({
+        success: false,
+        message: "User does not exist.",
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+}
