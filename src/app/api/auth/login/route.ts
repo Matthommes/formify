ada6ac53d25fc05c54e5faa97fd8ced71ff8ae2c
Matthew Akahomen <alexislordqtest@gmail.com>
2024-09-13 01:55:53 +0100

    Fix prisma error

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,22 +1,37 @@
-// app/api/auth/login/route.ts
+// app/api/auth/register/route.ts
 
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
   try {
     const { email, password } = await req.json();
+
+    // Check if the user already exists
     const user = await prisma.user.findUnique({
       where: { email },
     });
 
     if (user) {
-      return NextResponse.json({ success: false, message: "User does not exist." });
+      return NextResponse.json({
+        success: false,
+        message: "User already exists.",
+      });
     }
+
+    // Optionally, create a new user here if needed
+    // const newUser = await prisma.user.create({
+    //   data: { email, password }
+    // });
+
+    return NextResponse.json({
+      success: true,
+      message: "User registration successful.",
+    });
   } catch (error) {
     return NextResponse.json({
       success: false,
-      message: "Something went wrong",
+      message: "Something went wrong.",
     });
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export default function App() {
   return (
-    <main className="flex flex-col items-center justify-center p-8 h-[70vh] text-zinc">
+    <main className="flex flex-col items-center justify-center p-8 h-[70vh] text-zinc-800">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-4">Formify</h1>
         <p className="text-lg mb-8">
@@ -13,7 +13,7 @@ export default function App() {
           collection and streamline responses with ease.
         </p>
         <div className="flex justify-center items-center space-x-4">
-          <Button className=" text-white">
+          <Button className="text-white">
             <Link href="/register">Sign Up</Link>
           </Button>
           <Button variant="outline">
